71cc044271e35c37dd985dbf505204c501fb3d51
Ray Liu <rayliu109@gmail.com>
2019-01-29 20:59:26 -0500

    Improved code based on feedback

--- python/questions_test/chapter1_test.py
+++ python/questions_test/chapter1_test.py
@@ -1,35 +1,32 @@
-
 import pytest
 from questions import chapter1
 
+def test_question_1():
+    test_str = 'abc'
+    assert chapter1.is_unique(test_str) == True
 
+    test_str = 'cac'
+    assert chapter1.is_unique(test_str) == False
 
-def test_chapter_1_question_1():
-    test_str_1 = 'abc'
-    assert chapter1.is_unique(test_str_1) == True
+    test_str = 'asdfghjkl'
+    assert chapter1.is_unique(test_str) == True
 
-    test_str_2 = 'cac'
-    assert chapter1.is_unique(test_str_2) == False
+    test_str = ''
+    assert chapter1.is_unique(test_str) == True
 
-    test_str_3 = 'asdfghjkl'
-    assert chapter1.is_unique(test_str_3) == True
+def test_question_2():
+    test_str_a = 'abc'
+    test_str_b = 'abc'
+    assert chapter1.is_permutation(test_str_a,test_str_b) == True
 
-    test_str_4 = ''
-    assert chapter1.is_unique(test_str_4) == True
+    test_str_a = ''
+    test_str_b = 'abc'
+    assert chapter1.is_permutation(test_str_a,test_str_b) == False
 
-def test_chapter_1_question_2():
-    test_str_1_a = 'abc'
-    test_str_1_b = 'abc'
-    assert chapter1.is_permutation(test_str_1_a,test_str_1_b) == True
+    test_str_a = 'cab'
+    test_str_b = 'abc'
+    assert chapter1.is_permutation(test_str_a,test_str_b) == True
 
-    test_str_2_a = ''
-    test_str_2_b = 'abc'
-    assert chapter1.is_permutation(test_str_2_a,test_str_2_b) == False
-
-    test_str_3_a = 'cab'
-    test_str_3_b = 'abc'
-    assert chapter1.is_permutation(test_str_3_a,test_str_3_b) == True
-
-    test_str_4_a = 'ccab'
-    test_str_4_b = 'cabc'
-    assert chapter1.is_permutation(test_str_4_a,test_str_4_b) == True
+    test_str_a = 'ccab'
+    test_str_b = 'cabc'
+    assert chapter1.is_permutation(test_str_a,test_str_b) == True
